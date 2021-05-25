bb76a0dee2149242c54124ac7bfcfb5c35479e6f
jahanalem <s.r.alem19@gmail.com>
2021-05-25 16:28:44 +0200

    process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const api = require("./routes/pokemonRoute");
 
 const router = express.Router();
 const cors = require("cors");
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
