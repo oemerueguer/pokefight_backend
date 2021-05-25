983ea1abe3a7739eda31515a5e3bbd889f907765
jahanalem <s.r.alem19@gmail.com>
2021-05-25 15:53:26 +0200

    finish 5 steps

---
+++ routes/pokemonRoute.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const pokemonRouter = express.Router();
+const data = require("../pokedex.json");
+
+pokemonRouter.get("/pokemon", (req, res) => {
+  res.status(200).json(data);
+});
+
+pokemonRouter.get("/pokemon/:id", (req, res) => {
+  const id = req.params.id;
+  let result = data.find((item) => item.id === +id);
+  //console.log(result);
+  res.status(200).json(result);
+});
+
+pokemonRouter.get("/pokemon/:id/:info", (req, res) => {
+  const info = req.params.info;
+  const id = req.params.id;
+
+  let findTarget = "";
+  switch (info) {
+    case "name":
+      findTarget = "name";
+      break;
+    case "type":
+      findTarget = "type";
+      break;
+    case "base":
+      findTarget = "base";
+      break;
+  }
+
+  if (findTarget) {
+    let result = null;
+    let element = data.find((i) => i.id === +id);
+
+    element ? (result = element[findTarget]) : null;
+    result
+      ? res.status(200).json(result)
+      : res.status(404).send(`There is no ${findTarget}`);
+  } else {
+    res
+      .status(404)
+      .send(`There is no pokemon with id ${id} and info(${findTarget})!`);
+  }
+});
+
+module.exports = pokemonRouter;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const api = require("./routes/pokemonRoute");
+
+const router = express.Router();
+const cors = require("cors");
+const port = 3000 || process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(api);
+
+const server = app.listen(port, () => {
+  console.log("Server is running!");
+});
+
+module.exports = server;
